d3b4a562805f95df8cbc9a61fcb65e94edfe253c
Zeshan Tawa <zeshan.tawa98@gmail.com>
2023-03-07 15:52:32 +0000

    Complete avgsum assignment

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,12 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        int sum = 0;
+        for(int i: nums){
+            sum += i;
+        }
+        double sumDec = (double)sum;
+        double avg = sumDec/nums.length;
+        return avg;
     }
 }
